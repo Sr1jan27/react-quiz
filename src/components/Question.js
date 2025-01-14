eb8523ab7e25a579b3265ef044d1b8868a01ed4f
Sr1jan27 <singhsrijan809@gmail.com>
2025-01-14 18:22:20 +0530

    RENDERING ANSWER COMPONENT

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,15 @@
+import Answer from "./Answer"
 const Question = () =>{
-    return <div>Question</div>
+    return (
+        <div>
+            <div>Question</div>
+            <div>Answer</div>
+            <div>Answer</div>
+            <div>Answer</div>
+            <div>Answer</div>
+        </div>
+
+    )
 }
 
 export default Question;
