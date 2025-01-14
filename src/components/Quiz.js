a0f1ca42cff451aef9865fb96ca96456115d5c79
Sr1jan27 <singhsrijan809@gmail.com>
2025-01-14 21:34:56 +0530

    H

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import Question from "./Question";
 
 const Quiz = () =>{
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+    // let questionIndex = 0;
+
+    const testClick = () =>{
+        console.log("Test click");
+        setCurrentQuestionIndex(currentQuestionIndex+1);
+        // questionIndex++;
+        console.log(currentQuestionIndex);
+        
+    }
     return (
         <div className="quiz">
             <div>
                 <div className="score">Question 1/8 </div>
                 <Question />
-                <div className="next-button">Next question</div>
+                <div className="next-button" onClick={testClick}>
+                    Next question {currentQuestionIndex} </div>
             </div>
         </div>
     );
